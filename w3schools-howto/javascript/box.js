a1ac880399ac19f70fd07721648823fe19c02d78
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-22 13:40:13 -0300

    tab box

--- w3schools-howto/javascript/box.js
+++ w3schools-howto/javascript/box.js
@@ -1,26 +1,16 @@
 /** 
   * Javascript do box.html 
   */
- function openCity(evt, cityName){
-   //declare all variables
-   var i, tabcontent, tablinks;
-
-   //get all elements with class ="tabcontent" and hide them 
-   tabcontent = document.getElementsByClassName("tabcontent");
-
-   for(i = 0; i < tabcontent.length; i++){
-      tabcontent[i].style.display = "none";
-   }
-   
-   //get all elements with class = "tablinks" and remove the class "active"
-   tablinks = document.getElementsByClassName("tablinks");
-
-   for(i = 0; i < tablinks.length; i++){
-     tablinks[i].className = tablinks[i].className.replace("active", "");
-   }
-
-   //show the current tab, and add an "active" class to the button that opened the tab 
-   document.getElementById(cityName).style.display = "block";
-   evt.currentTarget.className += "active";
-  
- }
+ function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
