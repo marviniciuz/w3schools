f88cfbb388d663acd93fbf45528f46333a54c3ea
@douglasabnovato <douglasabnovato@gmail.com>
2019-11-18 15:45:33 -0300

    template

--- w3schools-css/foodblog-template/javascript/sidebar.js
+++ w3schools-css/foodblog-template/javascript/sidebar.js
@@ -1,8 +1,8 @@
 // Script to open and close sidebar
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.display = "block";
 }
-   
+ 
 function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("mySidebar").style.display = "none";
 }
